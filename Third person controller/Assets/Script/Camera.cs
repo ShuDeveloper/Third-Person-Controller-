7d83536ec149d76d9ab31b9cde439e1ffebe569f
Shubham Gautam <138363357+ShuDeveloper@users.noreply.github.com>
2023-12-11 15:56:35 +0530

    camera movement with player
    camera rotate 360

--- Third person controller/Assets/Script/Camera.cs
+++ Third person controller/Assets/Script/Camera.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class Camera : MonoBehaviour
 {
+    [SerializeField] float cameraSensitivity;
+    Transform player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = transform.parent;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float moveX = Input.GetAxis("Mouse X")*cameraSensitivity*Time.deltaTime;
+        player.Rotate(0,moveX,0);
     }
 }
