fabe851cc4906c7b80c8afcf1ff37c1b14f96687
Shubham Gautam <138363357+ShuDeveloper@users.noreply.github.com>
2023-12-11 17:10:10 +0530

    Update PlayerMovement.cs

--- Third person controller/Assets/Script/PlayerMovement.cs
+++ Third person controller/Assets/Script/PlayerMovement.cs
@@ -6,16 +6,18 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float moveSpeed;
     [SerializeField] float runSpeed;
-    [SerializeField] float WalkSpeed;
+    [SerializeField] float walkSpeed;
 
     Vector3 directions;
     Vector3 vilocity;
 
     CharacterController controller;
+    Animator animator;
     // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        animator = GetComponentInChildren<Animator>();    
     }
 
     // Update is called once per frame
@@ -23,8 +25,36 @@ public class PlayerMovement : MonoBehaviour
     {
         float moveZ = Input.GetAxis("Vertical");
         directions = new Vector3(0,0, moveZ);
-        directions *= moveSpeed*Time.deltaTime;
+
+        if(directions != Vector3.zero && !Input.GetKey(KeyCode.LeftShift))
+        {
+            walk();
+        }
+        else if (directions != Vector3.zero && Input.GetKey(KeyCode.LeftShift))
+        {
+            run();
+        }
+        else if (directions == Vector3.zero )
+        {
+            idle();
+        }
+        directions *= moveSpeed * Time.deltaTime;
         directions = transform.TransformDirection(directions);
         controller.Move(directions);
+    }
+    void walk()
+    {
+        animator.SetFloat("Move", 0.5f);
+        moveSpeed = walkSpeed;
+    }
+    void run()
+    {
+        animator.SetFloat("Move", 1f);
+        moveSpeed = runSpeed;
+    }
+    void idle()
+    {
+        animator.SetFloat("Move", 0.0f);
+      
     }
 }
