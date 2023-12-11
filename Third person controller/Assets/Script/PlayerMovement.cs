3182c6e627758d6f2703c057339d02f6f134a8e4
Shubham Gautam <138363357+ShuDeveloper@users.noreply.github.com>
2023-12-11 15:43:07 +0530

    Player movement
     W,S keys press so player move forward and backward ,

--- Third person controller/Assets/Script/PlayerMovement.cs
+++ Third person controller/Assets/Script/PlayerMovement.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] float moveSpeed;
+    [SerializeField] float runSpeed;
+    [SerializeField] float WalkSpeed;
+
+    Vector3 directions;
+    Vector3 vilocity;
+
+    CharacterController controller;
     // Start is called before the first frame update
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float moveZ = Input.GetAxis("Vertical");
+        directions = new Vector3(0,0, moveZ);
+        directions *= moveSpeed*Time.deltaTime;
+        controller.Move(directions);
     }
 }
